e2f798f863885f1a7c0283d4e501de7187d4cdde
potnurunarendragood <106417450+potnurunarendragood@users.noreply.github.com>
2026-05-30 16:06:46 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ region = "us-east-1"
 }
 
 resource "aws_instance" "name" {
-  ami           = "080beda09284d47e2"
+  ami           = "ami-080beda09284d47e2"
   instance_type = "t2.micro"
 
   tags = {
